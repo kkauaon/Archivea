619a4b07326626a38f7c3a1661cd4d1bcbad1cfd
Kauã Sousa <kaua.sousa63@aluno.ifce.edu.br>
2025-02-10 21:13:23 -0300

    Otimizações

--- Archivea/Views/FeedView.swift
+++ Archivea/Views/FeedView.swift
@@ -10,23 +10,25 @@ import SwiftUI
 struct FeedView: View {
     @State var search: String = ""
     
-    @State var fakeUser: [User] = [
+    var fakeUser: [User] = [
         User(name: "Kauã Sousa", handle: "kkauabr", bio: "entusiasta", isWhatsappPublic: true, createdAt: .now, avatar: "user1"),
         User(name: "João Gabriel", handle: "jotagezim", bio: "meu nome  joao grabiel tenho 17 anos e amo Jesus", isWhatsappPublic: false, createdAt: .now, avatar: "user2")
     ]
 //    
-    @State var fakePost : [Post] = [
-        .init(author: User(name: "João Gabriel", handle: "jotagezim", bio: "meu nome  joao grabiel tenho 17 anos e amo Jesus", isWhatsappPublic: false, createdAt: .now, avatar: "user1"), name: "camera cybershot dsc-w620", desc: "eu ganhei essa câmera de aniversário de 17 anos, da minha tia Claudia. amei!", photo: "cybershot", preservation: 5, fields: []),
-        .init(author: User(name: "Kauã Sousa", handle: "kkauabr", bio: "entuasista", isWhatsappPublic: false, createdAt: .now, avatar: "user2"), name: "disco legal", desc: "este disco foi encontrado na sede da ROckstar Games e é de extrema confidencialidade", photo: "disco", preservation: 5, fields: [.init(fieldName: "local", fieldValue: "porao da sede da rockstar")]),
-        .init(author: User(name: "João Gabriel", handle: "jotagezim", bio: "meu nome  joao grabiel tenho 17 anos e amo Jesus", isWhatsappPublic: false, createdAt: .now, avatar: "user1"), name: "manga haikyu!! cap01", desc: "eu ganhei essa câmera de aniversário de 17 anos, da minha tia Claudia. amei!", photo: "manga", preservation: 4, fields: []),
-        .init(author: User(name: "Kauã Sousa", handle: "kkauabr", bio: "entuasista", isWhatsappPublic: false, createdAt: .now, avatar: "user2"), name: "mewtwo ex edição limitada", desc: "este disco foi encontrado na sede da ROckstar Games e é de extrema confidencialidade", photo: "carta", preservation: 5, fields: [.init(fieldName: "local", fieldValue: "porao da sede da rockstar")])
-    ]
+    var fakePost : [Post] {
+        return [
+            .init(author: fakeUser[1], name: "Câmera Cybershot DSC-W620", desc: "eu ganhei essa câmera de aniversário de 17 anos, da minha tia Claudia. amei!", photo: "cybershot", preservation: 5, fields: []),
+            .init(author: fakeUser[0], name: "Jogo GTA VI PS5", desc: "este disco foi encontrado na sede da ROckstar Games e é de extrema confidencialidade", photo: "disco", preservation: 5, fields: [.init(fieldName: "local", fieldValue: "porao da sede da rockstar")]),
+            .init(author: fakeUser[1], name: "Mangá Haikyu!! 01", desc: "eu ganhei essa câmera de aniversário de 17 anos, da minha tia Claudia. amei!", photo: "manga", preservation: 4, fields: []),
+            .init(author: fakeUser[0], name: "Mewtwo EX #282 Genetic Apex", desc: "este disco foi encontrado na sede da ROckstar Games e é de extrema confidencialidade", photo: "carta", preservation: 5, fields: [.init(fieldName: "local", fieldValue: "porao da sede da rockstar")])
+        ]
+    }
     
     var body: some View {
         NavigationStack {
             ScrollView {
                 HStack(alignment: .top, spacing: 25) {
-                    VStack {
+                    LazyVStack {
                         ForEach (fakePost.indices, id: \.self) { position in
                             if position % 2 == 0 {
                                 NavigationLink(destination: PostExtendedView(post: fakePost[position])){
@@ -35,7 +37,7 @@ struct FeedView: View {
                             }
                         }
                     }
-                    VStack {
+                    LazyVStack {
                         ForEach (fakePost.indices, id: \.self) { position in
                             if position % 2 != 0 {
                                 NavigationLink(destination: PostExtendedView(post: fakePost[position])){
@@ -56,12 +58,6 @@ struct FeedView: View {
             .navigationTitle("Feed")
         }
         .searchable(text: $search, placement: .navigationBarDrawer(displayMode: .always), prompt: "Pesquise por um item...")
-        //Apenas colocando mais postagens-fake.
-        .onAppear {
-            for _ in 0...20 {
-                fakePost.append(fakePost.randomElement()!)
-            }
-        }
     }
 }
 
